08083a56cfea091bc182e8741f42e10ea284594c
Nitish Jaiswal <141999440+nitishjaiswal2002@users.noreply.github.com>
2024-04-20 20:57:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+// __define-ocg__
+// Get the first number from the user
+var number1 = prompt("Enter the first number:");
+
+// Get the second number from the user
+var number2 = prompt("Enter the second number:");
+
+// Convert the input strings to numbers
+number1 = Number(number1);
+number2 = Number(number2);
+
+// Compare the numbers and store the result in a variable
+var result = number1 === number2;
+
+// Display an alert with the result
+alert(result);
+
